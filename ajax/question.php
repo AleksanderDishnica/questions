479d883c60e485f96c8a84a5817e57a172f52ab6
Aleksander <adishnica@yahoo.com>
2020-05-03 22:42:07 +0200

    Added modals

--- ajax/question.php
+++ ajax/question.php
@@ -1,2 +1,6 @@
 <?php
-	echo 123;
+    if(isset($_POST['name'])){
+        $cookie_name = "cookie_name";
+        $cookie_value = $_POST['name'];
+        setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+    }
--- js/default.js
+++ js/default.js
@@ -1,5 +1,15 @@
 $(function() {
-	$('#start').click(function(e){
-		
+	$('#save').click(function(e){
+		let name = $('[name=name]').val();
+
+		$.ajax({
+			method:'POST',
+			url:'ajax/question.php',
+			data:{question:name},
+			success:function(data){
+				$('#exampleModal').modal('hide');
+				$('#example1Modal').modal();
+			}
+		});
 	});
 });
