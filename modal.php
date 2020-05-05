c3cac58f07f548fe294e8ab65a3df10f784acd9a
Aleksander <adishnica@yahoo.com>
2020-05-05 22:42:54 +0200

    Added modal.php

---
+++ modal.php
@@ -0,0 +1,46 @@
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+    	Name: <input type="text" name="name">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal" id="close">Close</button>
+        <button type="button" class="btn btn-primary" data-dismiss="modal" id="save">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div> 
+
+<!-- Modal -->
+<div class="modal fade" id="example1Modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+    	<input type="checkbox" id="answer1" name="answer" value="Bike">
+		<label for="answer"> Answer number 1</label><br>
+		<input type="checkbox" id="answer2" name="answer" value="Car">
+		<label for="answer"> Answer number 2</label><br>
+		<input type="checkbox" id="answer3" name="answer" value="Boat">
+		<label for="answer"> Answer number 3</label><br>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal" id="close">Close</button>
+        <button type="button" class="btn btn-primary" data-dismiss="modal" id="save1">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div> 
